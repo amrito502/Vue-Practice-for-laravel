40040b5408ec4bd6166fabacbc1118ba2dbc9c34
amrito502 <amritobosu7@gmail.com>
2024-05-16 15:24:46 +0600

    Vue Template Syntax

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+let msg = "Welcome Amrito!"
+let dynamicId = 2
+let isButtonDisabled = true
+const objectOfAttrs = {
+  id: "container",
+  class: "wrapper"
+}
+let seen = false
+let google = "https://google.com"
+const clickMe = () => {
+  console.log('Click')
+}
+</script>
+
+<template>
+  {{ msg }}
+  <div v-html="msg"></div>
+
+  <div v-bind:id="dynamicId">dynamicid</div>
+  <button :disabled="isButtonDisabled">button</button>
+  <div v-bind="objectOfAttrs">abc</div>
+  <p v-if="seen">Now you see me</p>
+  <p v-show="seen">Now you see me</p>
+  <a :href="google">Google</a> <br>
+  <button v-on:click="clickMe">Click Me</button>
+</template>
+
+<style scoped>
+
+</style>
