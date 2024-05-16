a647e55025a0ea2aa07c1b73c9c7b9458dbabd6c
amrito502 <amritobosu7@gmail.com>
2024-05-16 16:56:08 +0600

    using v-for

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <script setup>
-import { ref, reactive } from "vue";
-// let name = ref("Amrito Bosu")
+let friends = ["Asma", "Sakib", "joy", "rakib"];
 
-// let email = ref("");
-// let password = ref("");
-
-const form = reactive({
-  email: "",
-  password: ""
-})
-
+const products = [
+  {
+    name: "DataVista T Shirt",
+    description: "Lorem ipsum dolor, sit amet.",
+    price: 25.99,
+    category: "Apparel",
+  },
+  {
+    name: "Exploretech Hoddie",
+    description: "Lorem ipsum dolor, sit amet.",
+    price: 39.99,
+    category: "Apparel",
+  },
+  {
+    name: "Alpine Bottle",
+    description: "Lorem ipsum dolor, sit amet.",
+    price: 18.99,
+    category: "Accessories",
+  },
+];
 </script>
 
 <template>
-  <div>
-    <!-- <input type="text" v-model="name"><br> -->
-    <!-- <p>{{ name }}</p> -->
+    <!-- <li v-for="(friend, index) in friends" :key="index">{{ index }} {{friend}}</li> -->
 
-    <form action="">
-      <input type="text" v-model="form.email" name="email" placeholder="Enter Email" /><br />
-      <br />
-      <input type="text" v-model="form.password" name="password" placeholder="Enter Password" />
-    </form>
-    {{ form.email }} / {{ form.password }}
-  </div>
+    <div >
+      <table>
+        <tr>
+          <th>SL No.</th>
+          <th>Name :</th>
+          <th>Description :</th>
+          <th>Price :</th>
+          <th>Category :</th>
+        </tr>
+        <tr v-for="(product, index) in products" :key="index">
+          <td>{{ index }}</td>
+          <td>{{ product.name }}</td>
+          <td>{{ product.description }}</td>
+          <td>{{ product.price }}</td>
+          <td>{{ product.category }}</td>
+        </tr>
+      </table>
+    </div>
 </template>
 
 <style scoped>
+table,th,td{
+  border: 1px solid #000;
+}
 </style>
